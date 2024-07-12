8ee71276eeff1547f02a1fa2a5b8a7d5ea8a805d
Jonathan Hurd <hurd.jonathan@gmail.com>
2024-07-12 19:51:14 +0000

    chore: Move quickstart files out of media and update reference to them

--- src/extension.ts
+++ src/extension.ts
@@ -32,18 +32,18 @@ export async function activate(context: vscode.ExtensionContext) {
 }
 
 
-function changeVSCodeSetting(settingName: string, newValue: string) {
-  // Get the workspace configuration
-  const config = vscode.workspace.getConfiguration();
+// function changeVSCodeSetting(settingName: string, newValue: string) {
+//   // Get the workspace configuration
+//   const config = vscode.workspace.getConfiguration();
 
-  // Update the setting
-  config.update(settingName, newValue, vscode.ConfigurationTarget.Global)
-    .then(() => {
-      vscode.window.showInformationMessage(`Setting "${settingName}" updated to ${newValue}`);
-    }, (err) => {
-      vscode.window.showErrorMessage(`Failed to update setting: ${err}`);
-    });
-}
+//   // Update the setting
+//   config.update(settingName, newValue, vscode.ConfigurationTarget.Global)
+//     .then(() => {
+//       vscode.window.showInformationMessage(`Setting "${settingName}" updated to ${newValue}`);
+//     }, (err) => {
+//       vscode.window.showErrorMessage(`Failed to update setting: ${err}`);
+//     });
+// }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
