b9a323e6a63a1a820295f0cc0bd4ea6eeb640140
Jonathan Hurd <hurd.jonathan@gmail.com>
2024-07-30 16:51:50 -0500

    fix: Get the right .devcontainer path for local development

--- src/utils/getDevContainerPath.ts
+++ src/utils/getDevContainerPath.ts
@@ -1,11 +1,9 @@
 import * as vscode from "vscode";
 
 export default function getDevContainerPath(context: vscode.ExtensionContext) {
-  // this works for the vscode-quickstart but breaks for local development
-  return "/workspaces/vscode-quickstart/.devcontainer";
-  // if (context.extensionMode === vscode.ExtensionMode.Production) {
-  //   return "/workspaces/vscode-quickstart/.devcontainer";
-  // } else {
-  //   return "/workspaces/vscode-quickstart-extension/.devcontainer";
-  // }
+  if (context.extensionMode === vscode.ExtensionMode.Production) {
+    return "/workspaces/vscode-quickstart/.devcontainer";
+  } else {
+    return "/workspaces/vscode-quickstart-extension/.devcontainer";
+  }
 }
